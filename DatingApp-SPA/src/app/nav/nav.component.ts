100a82a3f0f41acae58d84392856cf761d805125
Neeraj Wadhwani <neewadhw@publicisgroupe.net>
2020-04-16 13:06:57 +0530

    Error handling completed

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -17,7 +17,7 @@ export class NavComponent implements OnInit {
         console.log('Login successful');
       },
       (error) => {
-        console.log('Login failed!!');
+        console.log(error);
       }
     );
   }
